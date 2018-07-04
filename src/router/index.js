7643a48aac79c86c6fd6f7e616ef8e6feaa2ab3f
cinyearchan <abaptx49684968@gmail.com>
2018-07-05 00:05:02 +0800

    0705

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,90 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import Index from '@/pages/Index/template.vue'
-import Login from '@/pages/Login/template.vue'
-import Register from '@/pages/Register/template.vue'
-import User from '@/pages/User/template.vue'
-import My from '@/pages/My/template.vue'
-import Edit from '@/pages/Edit/template.vue'
-import Detail from '@/pages/Detail/template.vue'
-import Create from '@/pages/Create/template.vue'
+// import Index from '@/pages/Index/template.vue'
+// import Login from '@/pages/Login/template.vue'
+// import Register from '@/pages/Register/template.vue'
+// import User from '@/pages/User/template.vue'
+// import My from '@/pages/My/template.vue'
+// import Edit from '@/pages/Edit/template.vue'
+// import Detail from '@/pages/Detail/template.vue'
+// import Create from '@/pages/Create/template.vue'
+
+import store from '../store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Index',
-      component: Index
+      component: () => import('@/pages/Index/template.vue')
     },
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: () => import('@/pages/Login/template.vue')
     },
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: () => import('@/pages/Register/template.vue')
     },
     {
-      path: '/user',
+      path: '/user/:userId',
       name: 'User',
-      component: User
+      component: () => import('@/pages/User/template.vue')
     },
     {
       path: '/my',
       name: 'My',
-      component: My
+      component: () => import('@/pages/My/template.vue'),
+      meta: {
+        requiresAuth: true
+      }
     },
     {
-      path: '/edit',
+      path: '/edit/:blogId',
       name: 'Edit',
-      component: Edit
+      component: () => import('@/pages/Edit/template.vue'),
+      meta: {
+        requiresAuth: true
+      }
     },
     {
-      path: '/detail',
+      path: '/detail/:blogId',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/Detail/template.vue')
     },
     {
       path: '/create',
       name: 'Create',
-      component: Create
+      component: () => import('@/pages/Create/template.vue'),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, form, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    store.dispatch('checkLogin').then(isLogin => {
+      if (!isLogin) {
+        next({
+          path: '/lgoin',
+          query: { 
+            redirect: to.fullPath
+          }
+        })
+      } else {
+        next()
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
